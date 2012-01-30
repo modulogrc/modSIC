7a9c243bd1ca7e2196ed1c2409408a25db374acf
Juan Carlos Castro y Castro <jcastro@modulo.com.br>
2012-01-30 18:47:18 -0200

    Connect check para Windows (usando WMI)

--- Modulo.Collect.Service/Infra/TargetCheckers/WindowsTargetChecker.cs
+++ Modulo.Collect.Service/Infra/TargetCheckers/WindowsTargetChecker.cs
@@ -3,14 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Modulo.Collect.Probe.Common;
+using System.Management;
 
 namespace Modulo.Collect.Service.Server.Infra
 {
     public class WindowsTargetChecker: ITargetChecker
     {
+        private bool IsThereCredential(Credentials credentials)
+        {
+            return ((credentials != null) && (credentials.Count > 0));
+        }
+
+        private ConnectionOptions GetConnectionOptions(Credentials credentials)
+        {
+            ConnectionOptions options = new ConnectionOptions();
+            options.Impersonation = ImpersonationLevel.Impersonate;
+            options.Authentication = AuthenticationLevel.Default;
+            options.EnablePrivileges = true;
+
+            if (this.IsThereCredential(credentials))
+            {
+                options.Username = credentials.GetFullyQualifiedUsername();
+                options.Password = credentials.GetPassword();
+            }
+
+            return options;
+        }
+
         public TargetCheckingResult Check(TargetInfo targetInfo)
         {
-            throw new NotImplementedException();
+            TargetCheckingResult retVal = new TargetCheckingResult();
+            bool success = false;
+
+            try
+            {
+                var targetAddress = targetInfo.GetAddress();
+                var credentials = targetInfo.IsLocalTarget() ? null : targetInfo.credentials;
+                var options = this.GetConnectionOptions(credentials);
+                var managementPath = new ManagementPath(string.Format(@"\\{0}\root\cimv2", targetAddress));
+                var connectScope = new ManagementScope() { Path = managementPath, Options = options };
+
+                connectScope.Connect();
+                success = true;
+            }
+            catch (Exception ex)
+            {
+                retVal.ErrorMessage = ex.Message;
+            }
+            finally
+            {
+                retVal.IsTargetAvailable = success;
+            }
+            return retVal;
         }
     }
 }
