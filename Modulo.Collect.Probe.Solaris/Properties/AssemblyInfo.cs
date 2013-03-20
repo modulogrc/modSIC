e319b53822742def337ab64a8b40432b79fd7433
luty81 <luty81@gmail.com>
2013-03-20 16:56:28 -0300

    Updating Release Notes and Assembly Version Number

--- Modulo.Collect.Probe.Solaris/Properties/AssemblyInfo.cs
+++ Modulo.Collect.Probe.Solaris/Properties/AssemblyInfo.cs
@@ -64,5 +64,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.7.0.203")]
-[assembly: AssemblyVersion("1.7.0.203")]
-[assembly: AssemblyFileVersion("1.7.11357.203")]
+[assembly: AssemblyVersion("1.7.5.0")]
+[assembly: AssemblyFileVersion("1.7.5.0")]
--- Modulo.Collect.Service.Contract/Properties/AssemblyInfo.cs
+++ Modulo.Collect.Service.Contract/Properties/AssemblyInfo.cs
@@ -67,5 +67,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.7.0.203")]
 
-[assembly: AssemblyVersion("1.7.0.203")]
-[assembly: AssemblyFileVersion("1.7.11357.203")]
+[assembly: AssemblyVersion("1.7.5.0")]
+[assembly: AssemblyFileVersion("1.7.5.0")]
